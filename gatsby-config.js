49838f7844c0e5eb7b9e1151d6caa4d2f391adf0
Brendan <roman.bp15@gmail.com>
2021-08-15 18:15:41 -0400

    add mdx plugin, sharp lib, fix filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.broman.app",
     title: "bproman.dev",
   },
   plugins: [
@@ -8,24 +8,41 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
-        path: `${__dirname}/pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: `${__dirname}/images`,
+        path: `${__dirname}/src/images`,
       },
     },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "posts",
-        path: `${__dirname}/posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".md", `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1035,
+              sizeByPixelDensity: true,
+            },
+          },
+        ],
       },
     },
     "gatsby-plugin-styled-components",
     "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
   ],
 };
