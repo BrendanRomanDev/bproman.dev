1a5399c84f17f5a09454b7a10da21c21f240e08f
Brendan <roman.bp15@gmail.com>
2021-08-15 17:55:06 -0400

    added gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,29 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "bproman.dev",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/pages`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/posts`,
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+  ],
 };
